d1aef2d1a4d4b89a3718067748151d3e21e5bd31
JustinBarfield <122563454+JustinBarfield@users.noreply.github.com>
2023-11-07 23:49:03 +0000

    commit

--- Busser.java
+++ Busser.java
@@ -1,10 +1,10 @@
 import java.util.ArrayList;
 
-public class Busser<E> extends Employee {
-    ArrayList<E> tables = new ArrayList<E>();
+public class Busser extends Employee {
+
+    ArrayList<Table> tables = new ArrayList<Table>();
+
     public Busser(String name, String position, boolean clockedIn) {
         super(name, position, clockedIn);
     }
-
-    
 }
